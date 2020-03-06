0eeb80483cd48fa70e1b4ffed6dac202694461c0
Kostya Kaganov <kkaganov@gmail.com>
2020-03-06 18:37:59 +0200

    Базовая реализация открытия модальной карты с помощью JS

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var link = document.querySelector('.mini-map');
+var popup = document.querySelector('.modal-map');
+var close = document.querySelector('.modal-map .modal-close');
+console.log(close);
+
+link.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+})
+
+close.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    popup.classList.remove('modal-show');
+})
+
+window.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+        if (popup.classList.contains('modal-show')) {
+            evt.preventDefault();
+            popup.classList.remove('modal-show');
+        }
+    }
+})
