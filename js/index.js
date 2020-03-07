4c382b26fa7450fa6a9a09b6e0dccd21edd0b891
Kostya Kaganov <kkaganov@gmail.com>
2020-03-07 23:58:07 +0200

    Завершение проекта

--- js/index.js
+++ js/index.js
@@ -6,6 +6,10 @@ var writeUsButton = document.querySelector('.write-us');
 var writeUsPopup = document.querySelector('.modal-contact');
 var writeUsClose = document.querySelector('.modal-contact .modal-close');
 
+var buyButtons = document.querySelectorAll('.buy');
+var buyPopup = document.querySelector('.modal-product-added');
+var buyClose = document.querySelector('.modal-product-added .modal-close');
+
 mapButton.addEventListener('click', function(evt) {
     evt.preventDefault();
     mapPopup.classList.add("modal-show");
@@ -45,3 +49,28 @@ window.addEventListener('keydown', function(evt) {
         }
     }
 })
+
+// Modal dialog 'Product Added To Cart'
+
+for (var i = 0; i < buyButtons.length; i++) {
+  buyButtons[i].addEventListener('click', onBuyButton);
+}
+
+buyClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  buyPopup.classList.remove('modal-show');
+})
+
+window.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 27) {
+      if (buyPopup.classList.contains('modal-show')) {
+          evt.preventDefault();
+          buyPopup.classList.remove('modal-show');
+      }
+  }
+})
+
+function onBuyButton(evt) {
+  evt.preventDefault();
+  buyPopup.classList.add("modal-show");
+}
