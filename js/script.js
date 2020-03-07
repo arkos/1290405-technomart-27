e3568a07a6fcbe0747a0d2792a74443cbd08a775
Kostya Kaganov <kkaganov@gmail.com>
2020-03-07 12:17:09 +0200

    Реализация открытия формы по кнопке Заблудились через JS

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
-var link = document.querySelector('.mini-map');
-var popup = document.querySelector('.modal-map');
-var close = document.querySelector('.modal-map .modal-close');
-console.log(close);
+var mapButton = document.querySelector('.mini-map');
+var mapPopup = document.querySelector('.modal-map');
+var mapClose = document.querySelector('.modal-map .modal-close');
 
-link.addEventListener('click', function(evt) {
+var writeUsButton = document.querySelector('.write-us');
+var writeUsPopup = document.querySelector('.modal-contact');
+var writeUsClose = document.querySelector('.modal-contact .modal-close');
+
+mapButton.addEventListener('click', function(evt) {
     evt.preventDefault();
-    popup.classList.add("modal-show");
+    mapPopup.classList.add("modal-show");
 })
 
-close.addEventListener('click', function(evt) {
+mapClose.addEventListener('click', function(evt) {
     evt.preventDefault();
-    popup.classList.remove('modal-show');
+    mapPopup.classList.remove('modal-show');
 })
 
 window.addEventListener('keydown', function(evt) {
     if (evt.keyCode === 27) {
-        if (popup.classList.contains('modal-show')) {
+        if (mapPopup.classList.contains('modal-show')) {
             evt.preventDefault();
-            popup.classList.remove('modal-show');
+            mapPopup.classList.remove('modal-show');
+        }
+    }
+})
+
+// Modal dialog 'Write Us'
+
+writeUsButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    writeUsPopup.classList.add("modal-show");
+})
+
+writeUsClose.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    writeUsPopup.classList.remove('modal-show');
+})
+
+window.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+        if (writeUsPopup.classList.contains('modal-show')) {
+            evt.preventDefault();
+            writeUsPopup.classList.remove('modal-show');
         }
     }
 })
